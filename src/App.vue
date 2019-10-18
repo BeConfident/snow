d9aeab61f1b66c3f29cde51a6f8cd5ab399ce52f
GepengCn <214764500@qq.com>
2019-10-19 01:55:19 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>梦马</span>
+        <span class="font-weight-light">户外</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank">
+        <span class="mr-2">图片</span>
+      </v-btn>
+      <v-btn
+              href="https://github.com/vuetifyjs/vuetify/releases/latest"
+              target="_blank">
+        <span class="mr-2">视频</span>
+      </v-btn>
+      <v-btn
+              href="https://github.com/vuetifyjs/vuetify/releases/latest"
+              target="_blank">
+        <span class="mr-2">直播</span>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
