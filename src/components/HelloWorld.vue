40863746440d516d7541eff78fbf174b3e975bb8
GepengCn <214764500@qq.com>
2019-11-12 22:11:21 +0800

    for循环显示

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,88 +1,21 @@
 <template>
   <v-container fluid>
-    <v-row justify="space-around">
+    <v-row justify="space-around" v-for="(item,index) in items" :key="index">
       <v-col cols="12">
         <v-card class="mx-auto">
-          <v-img class="white--text align-end"  src="../assets/one.jpg" max-height="500">
-            <v-card-title>Top 10 Australian beaches</v-card-title>
+          <v-img class="white--text align-end"  :src="item.src" max-height="500">
+            <v-card-title>{{item.title}}</v-card-title>
           </v-img>
-          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+          <v-card-subtitle class="pb-0">{{item.subtitle}}</v-card-subtitle>
           <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-            <div>Whitsunday Island, Whitsunday Islands</div>
+            <div>{{item.text}}</div>
           </v-card-text>
           <v-card-actions>
             <v-btn color="orange" text>
-              Share
+              分享
             </v-btn>
             <v-btn color="orange" text>
-              Explore
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="space-around">
-      <v-col cols="12">
-        <v-card class="mx-auto">
-          <v-img class="white--text align-end"  src="../assets/two.jpg" max-height="500">
-            <v-card-title>Top 10 Australian beaches</v-card-title>
-          </v-img>
-          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-            <div>Whitsunday Island, Whitsunday Islands</div>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="orange" text>
-              Share
-            </v-btn>
-            <v-btn color="orange" text>
-              Explore
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="space-around">
-      <v-col cols="12">
-        <v-card class="mx-auto">
-          <v-img class="white--text align-end"  src="../assets/three.jpg" max-height="500">
-            <v-card-title>Top 10 Australian beaches</v-card-title>
-          </v-img>
-          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-            <div>Whitsunday Island, Whitsunday Islands</div>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="orange" text>
-              Share
-            </v-btn>
-            <v-btn color="orange" text>
-              Explore
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="space-around">
-      <v-col cols="12">
-        <v-card class="mx-auto">
-          <v-img class="white--text align-end"  src="../assets/four.jpg" max-height="500">
-            <v-card-title>Top 10 Australian beaches</v-card-title>
-          </v-img>
-          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-            <div>Whitsunday Island, Whitsunday Islands</div>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="orange" text>
-              Share
-            </v-btn>
-            <v-btn color="orange" text>
-              Explore
+              了解更多
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -96,7 +29,32 @@ export default {
   name: 'HelloWorld',
 
   data: () => ({
-
+    items: [
+      {
+        src: 'one.jpg',
+        title:'Top 10 Australian beaches',
+        subtitle:'Number 10',
+        text:'Whitsunday Island, Whitsunday Islands',
+      },
+      {
+        src: 'two.jpg',
+        title:'Top 10 Australian beaches',
+        subtitle:'Number 10',
+        text:'Whitsunday Island, Whitsunday Islands',
+      },
+      {
+        src: 'three.jpg',
+        title:'Top 10 Australian beaches',
+        subtitle:'Number 10',
+        text:'Whitsunday Island, Whitsunday Islands',
+      },
+      {
+        src: 'four.jpg',
+        title:'Top 10 Australian beaches',
+        subtitle:'Number 10',
+        text:'Whitsunday Island, Whitsunday Islands',
+      },
+    ]
   }),
 };
 </script>
