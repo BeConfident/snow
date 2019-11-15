6bb4f29301eb2fa6ea4b4fd03b501674caf114a9
葛鹏 <gepeng@baiwang.com>
2019-11-15 16:31:09 +0800

    支付页面

--- src/views/pay.vue
+++ src/views/pay.vue
@@ -1,34 +1,26 @@
 <template>
     <v-container>
         <v-row dense>
-            <v-col
-                    v-for="(item, i) in items"
-                    :key="i"
-                    cols="12"
-            >
-                <v-card
-                        :color="item.color"
-                        dark
-                >
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                        <div>
-                            <v-card-title
-                                    class="headline"
-                                    v-text="item.title"
-                            ></v-card-title>
-
-                            <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+            <v-col v-for="(item, i) in items" :key="i" cols="12">
+                <v-hover v-slot:default="{ hover }">
+                    <v-card :elevation="hover? 12 : 5" :color="item.color">
+                        <div class="d-flex flex-no-wrap justify-space-between">
+                            <div>
+                                <v-card-title class="headline" v-text="item.artist"></v-card-title>
+                                <v-card-subtitle v-text="item.title"></v-card-subtitle>
+                            </div>
+                            <v-avatar class="ma-3" size="120" tile >
+                                <v-img :src="item.src"></v-img>
+                            </v-avatar>
                         </div>
-
-                        <v-avatar
-                                class="ma-3"
-                                size="125"
-                                tile
-                        >
-                            <v-img :src="item.src"></v-img>
-                        </v-avatar>
-                    </div>
-                </v-card>
+                    </v-card>
+                </v-hover>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="9"></v-col>
+            <v-col cols="3">
+                <v-btn bottom right block outlined color="primary">支付</v-btn>
             </v-col>
         </v-row>
     </v-container>
@@ -38,8 +30,7 @@
         data: () => ({
             items: [
                 {
-                    color: 'primary',
-                    src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+                    src: 'one.jpg',
                     title: '哈尔滨-亚布力',
                     artist: '¥800.0',
                 },
